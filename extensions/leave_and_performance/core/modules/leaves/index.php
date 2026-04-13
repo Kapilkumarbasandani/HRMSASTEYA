88f41248e49ab9ff96afd3a0b45f708fc3a5d601
Kapil <kapil@asteyaservices.com>
2026-04-13 18:57:41 +0530

    Admin Leave Management: show only All Employee Leaves tab, remove My Leaves and Subordinate tabs

--- extensions/leave_and_performance/core/modules/leaves/index.php
+++ extensions/leave_and_performance/core/modules/leaves/index.php
@@ -15,11 +15,10 @@ $isAdmin = ($user->user_level === 'Admin');
     <ul class="nav nav-tabs" id="modTab" style="margin-bottom:0px;margin-left:5px;border-bottom: none;">
         <?php if ($isAdmin) { ?>
         <li class="active"><a id="tabAllEmployeeLeave" href="#tabPageAllEmployeeLeave"><?=t('All Employee Leaves')?></a></li>
-        <li><a id="tabEmployeeLeave" href="#tabPageEmployeeLeave"><?=t('My Leaves')?></a></li>
         <?php } else { ?>
         <li class="active"><a id="tabEmployeeLeave" href="#tabPageEmployeeLeave"><?=t('My Leaves')?></a></li>
-        <?php } ?>
         <li><a id="tabSubEmployeeLeave" href="#tabPageSubEmployeeLeave"><?=t('Subordinate Leaves')?></a></li>
+        <?php } ?>
     </ul>
 
     <div class="tab-content">
@@ -29,10 +28,8 @@ $isAdmin = ($user->user_level === 'Admin');
             <div id="AllEmployeeLeaveForm"></div>
             <div id="AllEmployeeLeaveFilterForm"></div>
         </div>
-        <div class="tab-pane" id="tabPageEmployeeLeave">
         <?php } else { ?>
         <div class="tab-pane active" id="tabPageEmployeeLeave">
-        <?php } ?>
             <div id="EmployeeLeaveTable" class="reviewBlock" data-content="List" style="padding-left:5px;"></div>
             <div id="EmployeeLeaveForm"></div>
             <div id="EmployeeLeaveFilterForm"></div>
@@ -42,6 +39,7 @@ $isAdmin = ($user->user_level === 'Admin');
             <div id="SubEmployeeLeaveForm"></div>
             <div id="SubEmployeeLeaveFilterForm"></div>
         </div>
+        <?php } ?>
     </div>
 
 </div>
@@ -53,8 +51,8 @@ modJsList['tabAllEmployeeLeave'] = new AllEmployeeLeaveAdapter('EmployeeLeave','
 modJsList['tabAllEmployeeLeave'].setObjectTypeName('EmployeeLeave');
 modJsList['tabAllEmployeeLeave'].setAccess(['get','element','save','delete']);
 modJsList['tabAllEmployeeLeave'].setDataPipe(new IceDataPipe(modJsList['tabAllEmployeeLeave']));
-<?php } ?>
-
+var modJs = modJsList['tabAllEmployeeLeave'];
+<?php } else { ?>
 modJsList['tabEmployeeLeave'] = new EmployeeLeaveAdapter('EmployeeLeave','EmployeeLeave','employee=<?=$employeeId?>','date_start desc');
 modJsList['tabEmployeeLeave'].setObjectTypeName('EmployeeLeave');
 modJsList['tabEmployeeLeave'].setAccess(['get','element','save','delete']);
@@ -65,10 +63,6 @@ modJsList['tabSubEmployeeLeave'].setRemoteTable(true);
 modJsList['tabSubEmployeeLeave'].setObjectTypeName('EmployeeLeave');
 modJsList['tabSubEmployeeLeave'].setAccess(['get','element','save','delete']);
 modJsList['tabSubEmployeeLeave'].setDataPipe(new IceDataPipe(modJsList['tabSubEmployeeLeave']));
-
-<?php if ($isAdmin) { ?>
-var modJs = modJsList['tabAllEmployeeLeave'];
-<?php } else { ?>
 var modJs = modJsList['tabEmployeeLeave'];
 <?php } ?>
 
